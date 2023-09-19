afdb5e10cba00a6d8d539269c2d05d316e6fbdb0
raffialli <raffialli@gmail.com>
2023-09-18 20:48:59 -0400

    updated dirs

--- split_images.py
+++ split_images.py
@@ -2,27 +2,29 @@ import os
 import shutil
 from random import shuffle
 
-# Define source and destination directories
-src_dir = 'E:\\TensorFlow\\Front Door Vids\\training-files\\training\\non-human'
-val_dir = 'E:\\TensorFlow\\Front Door Vids\\training-files\\validation\\non-human'
-test_dir = 'E:\\TensorFlow\\Front Door Vids\\training-files\\testing\\non-human'
+# Define the original directory where the resized images are
+original_dir = 'E:\\TensorFlow\\backup\\training_images\\resize-human'
 
-# List all files in the source directory
-all_files = [f for f in os.listdir(src_dir) if os.path.isfile(os.path.join(src_dir, f))]
+# Define destination directories for training and validation
+train_dir = 'E:\\TensorFlow\\Front Door Vids\\training-files\\training\\human'
+val_dir = 'E:\\TensorFlow\\Front Door Vids\\training-files\\validation\\human'
+
+# List all files in the original directory
+all_files = [f for f in os.listdir(original_dir) if os.path.isfile(os.path.join(original_dir, f))]
 
 # Shuffle files randomly
 shuffle(all_files)
 
-# Calculate the number of files for training, validation, and testing
-num_train = int(len(all_files) * 0.8)
-num_val = int(len(all_files) * 0.1)
-num_test = len(all_files) - num_train - num_val  # Alternatively, int(len(all_files) * 0.1)
+# Calculate the number of files for training and validation
+num_train = int(len(all_files) * 0.9)
+num_val = len(all_files) - num_train
 
-# Move files
+# Move files to training and validation directories
 for i, file_name in enumerate(all_files):
     if i < num_train:
-        continue  # Skip files already in training
-    elif i < num_train + num_val:
-        shutil.move(os.path.join(src_dir, file_name), os.path.join(val_dir, file_name))
+        shutil.move(os.path.join(original_dir, file_name), os.path.join(train_dir, file_name))
     else:
-        shutil.move(os.path.join(src_dir, file_name), os.path.join(test_dir, file_name))
+        shutil.move(os.path.join(original_dir, file_name), os.path.join(val_dir, file_name))
+
+print(f"Moved {num_train} files to the training set.")
+print(f"Moved {num_val} files to the validation set.")
